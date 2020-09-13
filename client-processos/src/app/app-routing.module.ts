0cf84d949293f91a87ff38c1a7680f7c9a8ffc07
Luan Santos de souza <luan.souza@redetendencia.com.br>
2020-09-13 15:59:18 -0400

    iniciando o client angular 7

---
+++ client-processos/src/app/app-routing.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import {UsuarioComponent} from './usuarios/usuario/usuario.component';
+
+const routes: Routes = [
+  { path: '', component: UsuarioComponent},
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
---
+++ client-processos/src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import {ReactiveFormsModule} from '@angular/forms';
+import { UsuarioComponent } from './usuarios/usuario/usuario.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    UsuarioComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    ReactiveFormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
